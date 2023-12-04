6386328215f5c4db30e710a15cfafd0ac3dbe725
tamagoage <116415023+tamagoage@users.noreply.github.com>
2023-12-04 18:40:38 +0900

    create: php artisan model -a #8

---
+++ app/Http/Controllers/ShelfLifeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShelfLife;
+use App\Http\Requests\StoreShelfLifeRequest;
+use App\Http\Requests\UpdateShelfLifeRequest;
+
+class ShelfLifeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreShelfLifeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ShelfLife $shelfLife)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ShelfLife $shelfLife)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateShelfLifeRequest $request, ShelfLife $shelfLife)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ShelfLife $shelfLife)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TreatInterestController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TreatInterest;
+use App\Http\Requests\StoreTreatInterestRequest;
+use App\Http\Requests\UpdateTreatInterestRequest;
+
+class TreatInterestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTreatInterestRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TreatInterest $treatInterest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TreatInterest $treatInterest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTreatInterestRequest $request, TreatInterest $treatInterest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TreatInterest $treatInterest)
+    {
+        //
+    }
+}
