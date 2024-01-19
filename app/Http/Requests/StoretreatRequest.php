f8c35b62f14613329f1770c8baa8e5778bdf50b7
tamagoage <116415023+tamagoage@users.noreply.github.com>
2024-01-19 18:44:27 +0900

    fix: StoreTreatRequestのクラス芽衣を訂正 #29

--- app/Http/Requests/StoretreatRequest.php
+++ app/Http/Requests/StoretreatRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoretreatRequest extends FormRequest
+class StoreTreatRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
