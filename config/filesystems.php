e09bdb929315eb48de716221556f5dfb5cb9b72b
tamagoage <116415023+tamagoage@users.noreply.github.com>
2024-02-14 11:53:17 +0900

    Revert "add: configを設定 #36"
    This reverts commit ee1a4d5b615013f8a5446131deb7f14c9f6e92b9.
    
    laravel10ではfilesystems.phpをいじる必要がないため、revertする

--- config/filesystems.php
+++ config/filesystems.php
@@ -15,8 +15,6 @@ return [
 
     'default' => env('FILESYSTEM_DISK', 'local'),
 
-    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
-
     /*
     |--------------------------------------------------------------------------
     | Filesystem Disks
@@ -41,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
